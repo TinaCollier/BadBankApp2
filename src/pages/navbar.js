8dafd7cb7b27c5676462641d8e814961e8c6cd29
Tina Collier <tinacollier@Tinas-MacBook-Pro.local>
2022-08-04 19:25:38 -0400

    navbar edits

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -2,12 +2,13 @@ import CreateAccount from "./createaccount";
 
 function Nav(){
     return(
-        <nav className="navbar navbar-dark bg-transparent">
+        <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <ul className="nav">
+                <ul className="nav nav-tabs">
 
                     <li className="nav-item">
-                        <a className="nav-link" href="#/home/">Bad Bank</a>
+                        
+                        <a className="nav-link" href="#/home/"><img src="palmtreeicon.png" alt="" width="30" height="24" />Bad Bank</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#/createaccount/">Create Account</a>
